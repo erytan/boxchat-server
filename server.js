4b8259050d81f446ddb36766fd2ab7445b406486
erytan <ntan1695123@gmail.com>
2025-06-11 13:55:45 +0700

    upload server

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.get('/api/some-endpoint', (req, res) => {
 });
 const io = new Server(server, {
   cors: {
-    origin: ['https://boxchat-server.onrender.com/'],
+    origin: ['https://boxchat-server.onrender.com'],
     methods: ["GET", "POST"],
     credentials: true
   }
