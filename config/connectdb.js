1b576cde249f17699218f8f3fc498c4440074a85
erytan <ntan1695123@gmail.com>
2025-06-11 11:06:17 +0700

    update connectdb

--- config/connectdb.js
+++ config/connectdb.js
@@ -2,7 +2,7 @@ const { default: mongoose } = require('mongoose')
 mongoose.set('strictQuery', false)
 const dbConnect = async() => {
     try {
-        const conn = await mongoose.connect(process.env.MONGODB_URI)
+        const conn = await mongoose.connect("mongodb+srv://soemnho1695:soemnho1695@uocgiconguoiyeu.hyo2mgk.mongodb.net/BoxChat")
         if (conn.connection.readyState === 1)
             console.log('Connected to mongodb')
         else
